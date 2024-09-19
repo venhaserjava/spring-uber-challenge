f6b015280ccfe79481ae4489daf12f9a36ca879e
venhaserjava <venhaserjava@gmail.com>
2024-09-19 16:54:08 -0400

    Criando Controllers

---
+++ src/main/java/com/venhaserjava/spring_uber_challenge/SpringUberChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.venhaserjava.spring_uber_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUberChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringUberChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/venhaserjava/spring_uber_challenge/SpringUberChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.venhaserjava.spring_uber_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUberChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
